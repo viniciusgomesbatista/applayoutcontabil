d4db9138422e28ce9939f519a8255ee6cb8f4578
Vinicius Gomes <66857792+viniciusgomesbatista@users.noreply.github.com>
2024-02-28 18:09:12 -0300

    Update operacao.py

--- operacao.py
+++ operacao.py
@@ -221,7 +221,7 @@ def cria_zip_rateio(df_final, data_emissao, data_vencimento,select_tipo):
                     df_rateio['CRESP2'] = 'U' + df_rateio['CRESP1'].astype(str)
                     df_rateio['CRESP1'] = 'B' + df_rateio['CRESP1'].astype(str)
                     df_rateio['HISTÓRICO'] = 'H' + df_provisorio['HISTÓRICO'].iat[0]
-                    df_rateio['DATA'] = df_provisorio['DATA'].iat[0]
+                    df_rateio['DATA'] = pd.to_datetime(df_provisorio['DATA'].iat[0])
 
                     # Formatação de valores
                     soma_valor = df_rateio['VALOR'].sum()
